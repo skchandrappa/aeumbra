c798b0d8614c1f6dce2fe7cb55470f4ec3097c15
suchithkc <suchithkc@gmail.com>
2025-10-27 00:43:45 -0700

    Remove Railway configs, optimize for Render deployment

--- pythonbackend/build.sh
+++ pythonbackend/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Build script for Render
-pip install -r requirements.txt
+# Install with specific Python version if available
+python3.11 -m pip install --upgrade pip || python3 -m pip install --upgrade pip
+python3.11 -m pip install -r requirements.txt || python3 -m pip install -r requirements.txt
 
